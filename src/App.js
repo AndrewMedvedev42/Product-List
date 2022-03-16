f208c9603df22450898fc1fd34601fed2087ac6b
andrewNEW42 <sanjino56@gmail.com>
2022-03-16 15:36:07 +0200

    removed unused varibles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux"
-import { checkProductDataType } from "./modules/functions";
 import { loadProducts } from "./services/actions/postProduct";
 import axios from "axios";
 
